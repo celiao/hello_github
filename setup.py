565dd61f837c29e4e00cdb2d7612515aa106ffa6
Celia Oakley <celia.oakley@alumni.stanford.edu>
2013-11-14 12:47:20 -0800

    Created hello_github folder

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name = 'hello_github',
+    packages = ['.'],
+    version = '0.1',
+    install_requires = ['requests>=0.11.1'],
+    description = 'A test file',
+    author = 'Celia Oakley',
+    author_email = 'celia.oakley@alumni.stanford.edu',
+    url = 'https://github.com/celiao/hello_github',   # URL to github repo
+    download_url = 'https://github.com/celiao/hello_github/tarball/0.1',
+    keywords = ['testing', 'hello', 'example'],
+    classifiers = [],
+)
