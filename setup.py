1e3cc9a388fcb781c21cb8d75aa1ef5198a1c9b4
Celia Oakley <celia.oakley@alumni.stanford.edu>
2013-11-14 23:12:16 -0800

    Changed a couple things.

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-#def read(fname):
-#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 #with open('README.rst') as file:
 #    long_description = file.read()
 #
 
-def read(fname):
-    with open(fname) as fp:
-        content = fp.read()
-    return content
+#def read(fname):
+#    with open(fname) as fp:
+#        content = fp.read()
+#    return content
 
 setup(
     name = 'hello_github',
@@ -30,8 +30,8 @@ setup(
     download_url = 'https://github.com/celiao/hello_github/tarball/0.1',
     packages = ['hello_github'],
     #long_description=long_description,
-    #long_description=read('README.rst'),
-    long_description=(read("README.rst")),
+    long_description=read('README.rst'),
+    #long_description=(read("README.rst")),
     install_requires = ['requests>=0.11.1'],
     classifiers = [
         "Development Status :: 3 - Alpha",
